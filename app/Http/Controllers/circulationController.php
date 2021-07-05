e364a898dc1ff4e7bacdc6ad5c97d952e6458a42
alireza <sanreza6@live.com>
2021-07-06 00:20:24 +0430

    fix image upload

--- app/Http/Controllers/circulationController.php
+++ app/Http/Controllers/circulationController.php
@@ -63,13 +63,13 @@ class circulationController extends Controller
 
                 if ($i) {
                     $img = Image::make($file->getRealPath())->resize(100, 100);
-                    $img->save(public_path('../public_html/images/thumbnails/' . $fileName), 80);
+                    $img->save('/home1/shpourir/laravel/public_html/images/thumbnails/' . $fileName, 80);
 
                     $estate->thumbnail = $fileName;
                     $estate->save();
                     $i = false;
                 }
-                $file->move(public_path('../public_html/images'), $fileName);
+                $file->move('/home1/shpourir/laravel/public_html/images/', $fileName);
 
                 $estate->fresh()->images()->create([
                         'file_name' => $fileName,
